b7667206b6807700a73bd02e8a2f3acaa063ffc6
Iurii Ovsiannikov <jgoldste@z2t2c2.42bangkok.com>
2024-01-02 19:10:26 +0700

    Jan2_19-10

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: jgoldste <jgoldste@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:41:51 by jgoldste          #+#    #+#             */
-/*   Updated: 2023/12/28 19:33:47 by jgoldste         ###   ########.fr       */
+/*   Updated: 2023/12/28 19:39:15 by jgoldste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[]) {
 	if (argc > 2) {
-		std::cerr << "Usage: ./webserv or ./webserv [config_file_name].conf" << std::endl;
+		std::cerr << "Error: Usage: ./webserv or ./webserv [config_file_name].conf" << std::endl;
 		return 1;
 	}
 	std::string config_name("default.conf");
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]) {
 		Config config(config_name);
 	} catch (const Config::ConfigFileError& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
 	}
 	return 0;
 }
